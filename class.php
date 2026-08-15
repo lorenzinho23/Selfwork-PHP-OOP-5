a226493e91ea3dc0775eb4ed4e4493da33da0ecc
Lorenzo <lorenzo_tomasello@virgilio.it>
2026-08-15 12:50:47 +0200

    update o

---
+++ class.php
@@ -0,0 +1,42 @@
+<?php
+
+abstract class Category
+{
+    abstract public function getMyCategory();
+}
+
+class Attualita extends Category
+{
+    public function getMyCategory()
+    {
+        return "Attualità \n";
+    }
+}
+
+class Sport extends Category
+{
+    public function getMyCategory()
+    {
+        return "Sport \n";
+    }
+}
+
+class Gossip extends Category
+{
+    public function getMyCategory()
+    {
+        return "Gossip \n";
+    }
+}
+
+
+class Storia extends Category
+{
+    public function getMyCategory()
+    {
+        return "Storia \n";
+    }
+}
+
+$storia = new Storia();
+echo $storia->getMyCategory();
